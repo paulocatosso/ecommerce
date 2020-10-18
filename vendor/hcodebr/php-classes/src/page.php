10300b40ed8c0aa9b8b2c781e6b63184a96f9c98
paulocatosso <paulocatosso@gmail.com>
2020-10-18 11:39:12 -0300

    removendo arquivos de configuracao

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -19,7 +19,7 @@ class Page {
 
 			$config = array(
 					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache",
+					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 					"debug"         => false // set to false to improve the speed
 				   );
 			Tpl::configure( $config );
